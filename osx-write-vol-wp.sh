3fce2fe380a703a8a80af6982af3451ac3547288
Remon Lam <remon.lam@lamnet.nl>
2015-12-20 16:05:48 +0100

    fixes

--- osx-write-vol-wp.sh
+++ osx-write-vol-wp.sh
@@ -3,7 +3,7 @@ set -e
 
 # Script to workaround docker-machine/boot2docker OSX host volume issues: https://github.com/docker-library/mysql/issues/99
 
-echo '* Working around permission errors locally by making sure that "mysql" uses the same uid and gid as the host volume'
+echo '* Working around permission errors locally by making sure that "www-data" uses the same uid and gid as the host volume'
 TARGET_UID=$(stat -c "%u" /var/www/html)
 echo '-- Setting www-data user to use uid '$TARGET_UID
 usermod -o -u $TARGET_UID www-data || true
@@ -11,6 +11,7 @@ TARGET_GID=$(stat -c "%g" /var/www/html)
 echo '-- Setting www-data group to use gid '$TARGET_GID
 groupmod -o -g $TARGET_GID www-data || true
 echo
-echo '* Starting MySQL'
+echo '* Starting www-data'
 chown -R www-data:root /var/www/html/
 #/entrypoint.sh
+/entrypoint.sh apache2-foreground
