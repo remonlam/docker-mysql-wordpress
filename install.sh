2dd7a62e4962a8846afa89d8b413e0a8f9eeae35
Remon Lam <remon.lam@lamnet.nl>
2015-12-20 15:11:58 +0100

    fixes

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@
 export ROOT_PASSWORD=P@ssw0rd123
 export WORDPRESS_PASSWORD=P@ssw0rd123
 
+
 # Initial run, creat database
 docker run --detach \
   --name mysql \
@@ -13,10 +14,21 @@ docker run --detach \
   --env MYSQL_USER=wordpress \
   --env MYSQL_PASSWORD=$WORDPRESS_PASSWORD \
   --env MYSQL_DATABASE=wordpress \
-  --volume /Users/remonlam/docker/mounts/www1/mysql:/var/lib/mysql \
+  --volume /Users/remonlam/docker/mounts/testing1/mysql:/var/lib/mysql \
   mysql
 
+  /docker/mounts/mysql
+
 # Run MySQL container with already created database located in mountpoint
+docker run --detach \
+  --name mysql \
+  --env MYSQL_USER=wordpress \
+  --env MYSQL_PASSWORD=$WORDPRESS_PASSWORD \
+  --env MYSQL_DATABASE=wordpress \
+  --volume /Users/remonlam/docker/mounts/testing1/mysql:/var/lib/mysql \
+  mysql-osx
+
+
 docker run --detach \
   --name mysql \
   --env MYSQL_USER=wordpress \
@@ -31,7 +43,7 @@ docker run --detach \
 docker run --detach \
   --publish 80:80 \
   --name wordpress \
-  --volume /Users/remonlam/docker/mounts/www1/html:/var/www/html \
+  --volume /Users/remonlam/docker/mounts/testing1/wwwdata:/var/www/html \
   --link mysql:mysql \
   --env WORDPRESS_DB_USER=wordpress \
   --env WORDPRESS_DB_PASSWORD=$WORDPRESS_PASSWORD \
@@ -41,10 +53,26 @@ docker run --detach \
   docker run --detach \
     --publish 81:80 \
     --name wordpress-1 \
+    --volume /Users/remonlam/docker/mounts/testing1/wwwdata:/var/www/html \
     --link mysql:mysql \
     --env WORDPRESS_DB_USER=wordpress \
     --env WORDPRESS_DB_PASSWORD=$WORDPRESS_PASSWORD \
     wordpress
+
+    docker run --detach \
+      --publish 82:80 \
+      --name wordpress-2 \
+      --volume /Users/remonlam/docker/mounts/testing1/wwwdata:/var/www/html \
+      --link mysql:mysql \
+      --env WORDPRESS_DB_USER=wordpress \
+      --env WORDPRESS_DB_PASSWORD=$WORDPRESS_PASSWORD \
+      wordpress
+
+### haproxy
+#
+docker run -d -p 80:80 -p 1936:1936 --link wordpress-1 --link wordpress-2 tutum/haproxy
+docker run -d --link wordpress-1 --link wordpress-2 -e STATS_AUTH="auth:auth" -e STATS_PORT=1936 -p 80:80 -p 1936:1936 tutum/haproxy
+
 
 # Show running containers, there should be two of them runnint (mysql and wordpress).
 docker ps
